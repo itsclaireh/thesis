ae04f89f3cded949e45d52b5037428a55c579a74
claireballoon <hubacekcl@gmail.com>
2018-03-25 11:37:43 -0500

    changed csv values to be in quotes

--- load.py
+++ load.py
@@ -1,16 +1,31 @@
 import csv
 import numpy as np
 import urllib.request
+import scipy
+import pandas
+from io import StringIO
+from sklearn.preprocessing import MinMaxScaler
 
-with urllib.request.urlopen("https://raw.githubusercontent.com/claireballoon/thesis/master/TestData.tsv") as url:
-    raw_data = url.read().decode('utf-8')
-
-dataset = np.loadtxt(raw_data, delimiter="\t")
-X = dataset[:,0:1]
-y = dataset[:,2]
-
+url = "https://raw.githubusercontent.com/claireballoon/thesis/master/TestData.tsv"
 names=['id_str','text','relevant']
-categories=['1','2']
+dataframe = pandas.read_csv(url, sep='/t',engine='python')
 
-print(names)
-print(dataset)
+dataframe.shape
+
+
+#array = dataframe.values
+
+#print(array)
+
+#with urllib.request.urlopen("https://raw.githubusercontent.com/claireballoon/thesis/master/TestData.tsv") as url:
+#    raw_data = url.read().decode('utf-8')
+
+#dataset = np.loadtxt(raw_data, delimiter="\t")
+#X = dataset[:,0:1]
+#y = dataset[:,2]
+
+
+#categories=['1','2']
+
+#print(names)
+#print(dataset)
