54b3ff8be86c8ab83db784cffde0288e83ab799c
claireballoon <hubacekcl@gmail.com>
2018-04-27 00:52:56 -0500

    updated visualizations

---
+++ surveyVis/barPreventing.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
+
+def numResponses(df, responseNo):
+    return len(df.loc[df['preventingSH']==responseNo,['preventingSH']]);
+
+def barchartPrev(face,yout,insta,twit,snap,redd,tumbl,other):
+    objects = ('Facebook','YouTube','Instagram','Twitter','Snapchat','Reddit','Tumblr','Other');
+    #objects = ('Other','Tumblr','Reddit','Snapchat','Twitter','Instagram','YouTube','Facebook');
+    n_groups = len(objects);
+    index = np.arange(len(objects));
+    bar_width = 0.18;
+    opacity=0.8;
+    notadequate=(numResponses(face,'1'), numResponses(yout,'1'),numResponses(insta,'1'),numResponses(twit,'1'),numResponses(snap,'1'),numResponses(redd,'1'),numResponses(tumbl,'1'), numResponses(other,'1'));
+    some=(numResponses(face,'2'), numResponses(yout,'2'),numResponses(insta,'2'),numResponses(twit,'2'),numResponses(snap,'2'),numResponses(redd,'2'),numResponses(tumbl,'2'), numResponses(other,'2'));
+    adequate=(numResponses(face,'3'), numResponses(yout,'3'),numResponses(insta,'3'),numResponses(twit,'3'),numResponses(snap,'3'),numResponses(redd,'3'),numResponses(tumbl,'3'), numResponses(other,'3'));
+    idk=(numResponses(face,'4'), numResponses(yout,'4'),numResponses(insta,'4'),numResponses(twit,'4'),numResponses(snap,'4'),numResponses(redd,'4'),numResponses(tumbl,'4'), numResponses(other,'4'));
+
+    rects1=plt.bar(index, notadequate, bar_width,
+         alpha=opacity,
+         color='#450a5cff',
+         label='Not Adequate');
+    rects1=plt.bar(index+bar_width, some, bar_width,
+         alpha=opacity,
+         color='#2d6e8eff',
+         label='Some');
+    rects1=plt.bar(index+bar_width*2, adequate, bar_width,alpha=opacity,color='#49be6eff',label='Adequate');
+    rects1=plt.bar(index+bar_width*3, idk, bar_width,alpha=opacity,color='#e1e329ff',label='IDK');
+
+
+    plt.xlabel('Social Media Platform');
+    plt.ylabel('Number of Responses');
+    plt.title('Evaluation of Social Media\'s Mechanisms \nFor Preventing Sexual Harassment')
+    plt.xticks(index+bar_width*1.5, objects);
+    plt.legend();
+    plt.tight_layout();
+
+    plt.savefig("barPreventing.pdf", bbox_inches="tight");
+    plt.savefig("barPreventing.png", bbox_inches="tight");
+    plt.show();
---
+++ surveyVis/plot.py
@@ -0,0 +1,59 @@
+from barPreventing import barchartPrev
+from barReporting import barchartRep
+from barStackReporting import barStackRep
+from barStackPreventing import barStackPrev
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt
+#import matplotlib.backends.backend._pdf
+
+def socialMediaResults(df,no):
+    return df.loc[df['socialMedia']==no,['socialMedia','socialMediaOption','reportingSH','preventingSH']];
+
+def socialMediaResults2(df,no):
+    return df.loc[df['socialMedia']==no,['age','socialMedia','socialMediaOption','reportingSH','preventingSH']];
+
+df = pd.read_csv('survey-num.csv',header=None,index_col=None,encoding='utf8',dtype='str');
+#df.columns=['id_str','text','related','stance','category'];
+columns1=['startDate','endDate','responseType','progress','duration','finished','recordedDate','responseID','distributionChannel','language','consent']
+columns2=['age','gender','genderOption','staringLeering','catcalling','pinchingPoking','sexistComments','inappropriateDrawings','messagesStrangers','lewdRemarks','obsceneGestures','repeatedMessages','personalQuestions','stalkingHarassment','gropingTouching','unsolicitedPhotos','behaviorOptionSeverity','behaviorOptionText','socialMedia','socialMediaOption','preventingSH','reportingSH','improvments','improvementsOption'];
+columns=columns1+columns2;
+df.columns = np.ravel(columns);
+
+metadata=df[columns1];
+metadata = metadata.replace(np.nan,'',regex=True)
+#print(metadata);
+
+responses=df[columns2];
+responses = responses.replace(np.nan,'',regex=True)
+#print(responses);
+
+face = socialMediaResults(responses,'1');
+yout = socialMediaResults(responses,'2');
+insta = socialMediaResults(responses,'3');
+twit = socialMediaResults(responses,'4');
+snap = socialMediaResults(responses,'5');
+redd = socialMediaResults(responses,'6');
+tumbl = socialMediaResults(responses,'7');
+other = responses.loc[responses['socialMediaOption']!='',['socialMediaOption','preventingSH','reportingSH']];
+
+barchartPrev(face,yout,insta,twit,snap,redd,tumbl,other);
+barchartRep(face,yout,insta,twit,snap,redd,tumbl,other);
+barStackRep(face,yout,insta,twit,snap,redd,tumbl,other);
+barStackPrev(face,yout,insta,twit,snap,redd,tumbl,other);
+answer = df[['improvementsOption']];
+#print(answer);
+answer = answer.replace(np.nan,'',regex=True)
+ans = answer.loc[answer['improvementsOption']!='',['improvementsOption']];
+
+for i, row in ans.iterrows():
+    print(row.improvementsOption);
+    print('\n');
+
+face = socialMediaResults2(responses,'1');
+yout = socialMediaResults2(responses,'2');
+insta = socialMediaResults2(responses,'3');
+twit = socialMediaResults2(responses,'4');
+snap = socialMediaResults2(responses,'5');
+redd = socialMediaResults2(responses,'6');
